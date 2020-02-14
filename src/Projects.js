c256db84e3d7da6d350b3484d5680e8eb458ce66
wptechprodigy <waheed.freelancer16@gmail.com>
2020-02-14 16:16:35 +0100

    add component to display each project

--- src/Projects.js
+++ src/Projects.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import PROJECTS from './data/projects';
 
+class Project extends Component {
+  render() {
+    const { title, description, image, link } = this.props.project;
+
+    return (
+      <div style={{ display: 'inline-block', width: 300, margin: 10 }}>
+        <h3>{title}</h3>
+        <img src={image} alt="profile" style={{ width: 250, height: 150 }} />
+        <p>{description}</p>
+        <a href={link}>{link}</a>
+      </div>
+    );
+  }
+}
+
 class Projects extends Component {
   render() {
     return (
@@ -8,7 +23,7 @@ class Projects extends Component {
         <h2>Highlighted Projects</h2>
         <div>
           {PROJECTS.map(PROJECT => (
-            <div>{PROJECT.title}</div>
+            <Project key={PROJECT.id} project={PROJECT} />
           ))}
         </div>
       </div>
