52f62eb6e696403a9b29fdb7ecb1fd55d8f9a3ba
wptechprodigy <waheed.freelancer16@gmail.com>
2020-02-14 16:17:43 +0100

    add social profile list

---
+++ src/data/socialProfiles.js
@@ -0,0 +1,29 @@
+import githubIcon from '../assets/github_icon.png';
+import linkedinIcon from '../assets/linkedin_icon.png';
+import twitterIcon from '../assets/twitter_icon.png';
+import emailIcon from '../assets/email_icon.png';
+
+const SOCIAL_PROFILES = [
+  {
+    id: 1,
+    link: 'https://github.com/wptechprodigy',
+    image: githubIcon
+  },
+  {
+    id: 2,
+    link: 'https://www.linkedin.com/in/waheed-afolabi/',
+    image: linkedinIcon
+  },
+  {
+    id: 3,
+    link: 'https://twitter.com/checkwithwaheed',
+    image: twitterIcon
+  },
+  {
+    id: 4,
+    link: 'mailto:waheed.remote247@gmail.com',
+    image: emailIcon
+  }
+];
+
+export default SOCIAL_PROFILES;
