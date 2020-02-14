f117d5c5214e274e7024e2c2d8aaa4274c86bc16
wptechprodigy <waheed.freelancer16@gmail.com>
2020-02-14 12:55:07 +0100

    add more info about me

--- src/App.js
+++ src/App.js
@@ -6,7 +6,15 @@ class App extends Component {
       <div>
         <h1>Hello!</h1>
         <p>My name is Waheed. I'm a software engineer.</p>
-        <p>I'm always looking forward to working on meaningful better-life-enhancing projects.</p>
+        <p>
+          I'm always looking forward to working on meaningful
+          better-life-enhancing projects.
+        </p>
+        <div>
+          <p>I live in Lagos, Nigeria and code every day.</p>
+          <p>My favourite language is Javascript, and I also do Rust as well as GO.</p>
+          <p>Besides coding, I am a loving father. I love to read, play soccer and watch movies!</p>
+        </div>
       </div>
     );
   }
