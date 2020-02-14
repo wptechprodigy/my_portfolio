fd1d218fb3c0980fc7d4bb05d10ff05f37aa818b
wptechprodigy <waheed.freelancer16@gmail.com>
2020-02-14 10:41:06 +0100

    add basic app setup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ class App extends Component {
   render() {
     return (
       <div>
-        
+        <h1>Hello!</h1>
+        <p>My name is Waheed. I'm a software engineer.</p>
+        <p>I'm always looking forward to working on meaningful better-life-enhancing projects.</p>
       </div>
     );
   }
