b228a4765910585f09435433f0554524823ea461
wptechprodigy <waheed.freelancer16@gmail.com>
2020-02-14 13:05:01 +0100

    control amount of personal info shown by default

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  state = {
+    displayBio: false,
+  };
+
   render() {
     return (
       <div>
@@ -10,11 +14,19 @@ class App extends Component {
           I'm always looking forward to working on meaningful
           better-life-enhancing projects.
         </p>
-        <div>
-          <p>I live in Lagos, Nigeria and code every day.</p>
-          <p>My favourite language is Javascript, and I also do Rust as well as GO.</p>
-          <p>Besides coding, I am a loving father. I love to read, play soccer and watch movies!</p>
-        </div>
+        {this.state.displayBio ? (
+          <div>
+            <p>I live in Lagos, Nigeria and code every day.</p>
+            <p>
+              My favourite language is Javascript, and I also do Rust as well as
+              GO.
+            </p>
+            <p>
+              Besides coding, I am a loving father. I love to read, play soccer
+              and watch movies!
+            </p>
+          </div>
+        ) : null}
       </div>
     );
   }
